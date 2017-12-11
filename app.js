e439cdb085679d12d6146ab135474c542943aa26
unknown <freevibes@gmail.com>
2017-12-11 14:15:35 +0100

    Socket UI msg´s from client translates to OSC to udp local port

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ var express = require('express');
 var app = express();  
 var server = require('http').createServer(app);  
 var io = require('socket.io')(server);
+var osc = require('osc-min'),
+    dgram = require('dgram'),
+    remote;
 
+////////////////////////////////////////////////////////////////////////
 server.listen(8000);
 
 app.use(express.static(__dirname + '/node_modules'));  
@@ -11,11 +15,31 @@ app.get('/', function(req, res,next) {
     res.sendFile(__dirname + '/index.html');
 });
 app.use(express.static('public'));
-////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////	
+	
+// OSC
+var udp = dgram.createSocket('udp4', function(msg, rinfo) {
+	console.log(msg, rinfo);
+});
+
+
+
+/////IO CLIENT MSGs
 io.on('connection', function (socket){
-	console.log("user "+socket.id + " connected");
-	socket.on('Sosc1',function(data){
+console.log("user "+socket.id + " connected");
+	
+socket.on('Sosc1',function(data){
 		console.log("/Sosc1 "+data);
+			var x = osc.toBuffer({
+			oscType: 'message',
+				address: '/love',
+				args: [{
+					type: 'float',
+					value: data
+								}]
+			});
+		udp.send(x, 0 , x.length, 3333, "localhost");
+		
 	});
 	socket.on('Sosc2',function(data){
 		console.log("/Sosc2 "+data);
@@ -23,4 +47,3 @@ io.on('connection', function (socket){
 	});
 	
 });
-
