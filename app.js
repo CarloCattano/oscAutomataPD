faa047d908e530e953477cd41a3ea44fbac30a02
unknown <freevibes@gmail.com>
2017-12-12 01:18:23 +0100

    Better UI and osc IO´s

--- app.js
+++ app.js
@@ -16,13 +16,10 @@ app.get('/', function(req, res,next) {
 });
 app.use(express.static('public'));
 ////////////////////////////////////////////////////////	
-	
-// OSC
+
 var udp = dgram.createSocket('udp4', function(msg, rinfo) {
 	console.log("msg : " + msg +"Remote : " + rinfo);
 });
-
-
 
 /////IO CLIENT MSGs
 io.on('connection', function (socket){
@@ -74,7 +71,40 @@ socket.on('kickVol',function(data){
 		udp.send(micVol, 0 , micVol.length, 3333, "localhost");
 	});
 	socket.on('seqNotes',function(data){
-		
+		var seqSldVal = parseFloat(data.value);
+		var seqIndex = data.index + 1;
+		var seqOscMsg = "/s" + seqIndex;
+		var seqNotes = osc.toBuffer({
+			oscType: 'message',
+				address: seqOscMsg.toString(),
+				args: [{
+					type: 'float',
+					value: seqSldVal
+								}]
+			});
+		udp.send(seqNotes, 0 , seqNotes.length, 3333, "localhost");
+	});
+	socket.on('seqGate',function(data){
+		var micVol = osc.toBuffer({
+			oscType: 'message',
+				address: '/seqGate',
+				args: [{
+					type: 'float',
+					value: data
+								}]
+			});
+		udp.send(micVol, 0 , micVol.length, 3333, "localhost");
+	});
+	socket.on('seqFLT',function(data){
+		var seqFLT = osc.toBuffer({
+			oscType: 'message',
+				address: '/seqFLT',
+				args: [{
+					type: 'float',
+					value: data
+								}]
+			});
+		udp.send(seqFLT, 0 , seqFLT.length, 3333, "localhost");
 	});
 	
 });
